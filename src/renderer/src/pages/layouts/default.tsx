b7ea180826668eee074f02ab6d66ceb8774a0f45
Murilo Mathias Zelic <murilozelic@gmail.com>
2024-12-15 18:58:03 -0800

    Configurando Layout

--- src/renderer/src/pages/layouts/default.tsx
+++ src/renderer/src/pages/layouts/default.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 
-import { Sidebar } from 'phosphor-react'
+import { Sidebar } from '../../components/Sidebar'
 import { Header } from '../../components/Header'
 
 export function Default() {
