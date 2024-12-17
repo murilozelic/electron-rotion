9fa62b25e4fdd0f99630bf5b65903b0abcbb0033
Murilo Mathias Zelic <murilozelic@gmail.com>
2024-12-16 20:53:48 -0800

    Editor de texto

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,3 +1,4 @@
+import { Editor } from '../components/Editor'
 import { ToC } from '../components/ToC'
 
 export function Document() {
@@ -16,7 +17,9 @@ export function Document() {
         </ToC.Root>
       </aside>
 
-      <section className="flex-1 flex flex-col items-center">asdasd</section>
+      <section className="flex-1 flex flex-col items-center">
+        <Editor />
+      </section>
     </main>
   )
 }
