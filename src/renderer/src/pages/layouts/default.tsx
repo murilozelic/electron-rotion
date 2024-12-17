050b621707cbf0c83472f2a7649cdde19152f41b
Murilo Mathias Zelic <murilozelic@gmail.com>
2024-12-16 21:12:35 -0800

    Slidebar flexivel

--- src/renderer/src/pages/layouts/default.tsx
+++ src/renderer/src/pages/layouts/default.tsx
@@ -1,17 +1,25 @@
+import * as Collapsible from '@radix-ui/react-collapsible'
 import { Outlet } from 'react-router-dom'
 
 import { Sidebar } from '../../components/Sidebar'
 import { Header } from '../../components/Header'
+import { useState } from 'react'
 
 export function Default() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+
   return (
-    <div className="h-screen w-screen text-rotion-100 flex">
+    <Collapsible.Root
+      defaultOpen
+      onOpenChange={setIsSidebarOpen}
+      className="h-screen w-screen text-rotion-100 flex"
+    >
       <Sidebar />
       <div className="flex-1 flex flex-col max-h-screen">
-        <Header />
+        <Header isSidebarOpen={isSidebarOpen} />
 
         <Outlet />
       </div>
-    </div>
+    </Collapsible.Root>
   )
 }
